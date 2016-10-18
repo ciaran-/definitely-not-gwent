3327c285b9e066229d28effdd652484b51d5e83f
Ciaran McGhie <ciaranmcghie@gmail.com>
2016-10-18 13:25:14 +0100

    Some refactoring of routing out of the main service object

--- src/main/scala/APIService.scala
+++ src/main/scala/APIService.scala
@@ -1,22 +1,16 @@
 import akka.actor.ActorSystem
 import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpEntity, HttpResponse, StatusCodes}
+import akka.http.scaladsl.model.{HttpEntity, HttpResponse}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import com.typesafe.config.{Config, ConfigFactory}
 import spray.json.DefaultJsonProtocol
-import spray.json.PrettyPrinter
-import spray.json._
+
 
 import scala.concurrent.ExecutionContextExecutor
-import scala.util.{Failure, Success}
 
-trait Protocols extends DefaultJsonProtocol {
-  implicit val factionFormat = jsonFormat4(Faction)
-}
-
-trait Service extends Protocols{
+trait Service {
   implicit val system: ActorSystem
   implicit def executor: ExecutionContextExecutor
   implicit val materializer: ActorMaterializer
@@ -28,8 +22,9 @@ trait Service extends Protocols{
 object APIService extends App
                   with Service
                   with CorsSupport
+                  with FactionsRouting
 {
-  override implicit val system = ActorSystem()
+  override implicit val system = ActorSystem("definitely-not-gwent-data")
   override implicit val executor = system.dispatcher
   override implicit val materializer = ActorMaterializer()
 
@@ -43,21 +38,7 @@ object APIService extends App
           complete {
             HttpResponse(entity = HttpEntity(s"Hi! I'm an empty endpoint...for now."))
           }
-        } ~
-        pathPrefix("factions") {
-          pathEndOrSingleSlash {
-            onComplete(FactionsHelper.listAll()){
-              case Success(factions) =>
-                complete {
-                  HttpResponse(entity = HttpEntity(PrettyPrinter(factions.toJson)))
-                }
-              case Failure(exc) =>
-                complete {
-                  HttpResponse(status = StatusCodes.InternalServerError, entity = HttpEntity(s"error: ${exc.getMessage}"))
-                }
-            }
-          }
-        }
+        } ~ factionsRouting
       }
     }
   }
--- src/main/scala/FactionsHelper.scala
+++ src/main/scala/FactionsHelper.scala
@@ -1,6 +1,13 @@
 import com.typesafe.config.ConfigFactory
 import BetterPostgresDriver.api._
+import akka.http.scaladsl.model.{HttpEntity, HttpResponse, StatusCodes}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.PathMatchers.IntNumber
+import spray.json.PrettyPrinter
+import spray.json._
+
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 
 object FactionsHelper {
@@ -18,6 +25,11 @@ object FactionsHelper {
     db.run(clinics.result).map(_.map(x => x))
   }
 
+  def factionById(id: Int): Future[Option[Faction]] = {
+    db.run(
+      clinics.filter(_.id === id).result).map(_.headOption)
+  }
+
 }
 
 case class Faction(id: Int, name: String, description: String, logo: String)
@@ -30,4 +42,39 @@ class FactionsTable(tag: Tag) extends Table[Faction](tag, "factions"){
 
 
   def * =(id, name, description, img) <> ((Faction.apply _).tupled, Faction.unapply)
+}
+
+trait FactionsRouting extends DefaultJsonProtocol {
+  implicit val factionFormat = jsonFormat4(Faction)
+
+  val factionsRouting =
+    pathPrefix("factions") {
+      pathEndOrSingleSlash {
+        onComplete(FactionsHelper.listAll()){
+          case Success(factions) =>
+            complete {
+              HttpResponse(entity = HttpEntity(PrettyPrinter(factions.toJson)))
+            }
+          case Failure(exc) =>
+            complete {
+              HttpResponse(status = StatusCodes.InternalServerError, entity = HttpEntity(s"error: ${exc.getMessage}"))
+            }
+        }
+      } ~ {
+        path(IntNumber) { factionId =>
+          onComplete(FactionsHelper.factionById(factionId)) {
+            case Success(maybeFaction) =>
+              complete {
+                maybeFaction match {
+                  case Some(faction) =>
+                    HttpResponse(entity = HttpEntity(PrettyPrinter(faction.toJson)))
+                  case None =>
+                    HttpResponse(StatusCodes.NotFound, entity = s"Faction $factionId Not Found")
+                }
+              }
+            case Failure(exc) => complete(HttpResponse(StatusCodes.InternalServerError, entity = s"An error occurred ${exc.getMessage}"))
+          }
+        }
+      }
+    }
 }
